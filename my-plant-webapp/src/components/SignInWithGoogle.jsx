8928a83d192276bdc10767c222240d183308f25e
Top <tammasorn.tammasorn@mail.kmutt.ac.th>
2025-04-24 21:24:33 +0700

    Add toast popup after successfully logged in with Google

--- my-plant-webapp/src/components/SignInWithGoogle.jsx
+++ my-plant-webapp/src/components/SignInWithGoogle.jsx
@@ -1,5 +1,6 @@
 import React from "react";
 import { useNavigate } from "react-router-dom";
+import { toast } from "react-toastify";
 import { GoogleAuthProvider, signInWithPopup } from "firebase/auth";
 import { auth } from "../firebase/config.js";
 
@@ -10,7 +11,12 @@ const SignInWithGoogle = () => {
     signInWithPopup(auth, provider).then(async (result) => {
       console.log(result);
       if (result.user) {
-        navigate("/dashboard");
+        toast.success("User Logged in with Google Successfully!!", {
+          position: "top-center",
+        });
+        setTimeout(() => {
+          navigate("/dashboard");
+        }, 5000);
       }
     });
   };
