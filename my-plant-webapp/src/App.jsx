89341ffba5f87021edf2a20c507acc69de885524
Top <tammasorn.tammasorn@mail.kmutt.ac.th>
2025-05-01 00:41:05 +0700

    refactor(routing): restructure routing to group main page routes by layout using a MainLayout

--- my-plant-webapp/src/App.jsx
+++ my-plant-webapp/src/App.jsx
@@ -8,15 +8,10 @@ import {
   RouterProvider,
 } from "react-router-dom";
 
-import { auth } from "./firebase/config.js";
-
 // pages
 import RegisterPage from "./pages/RegisterPage";
 import LoginPage from "./pages/LoginPage";
 import DashboardPage from "./pages/DashboardPage";
-
-// components
-import SideBar from "./components/SideBar";
 
 // layouts
 import MainLayout from "./layouts/MainLayout";
@@ -24,19 +19,17 @@ import MainLayout from "./layouts/MainLayout";
 const App = () => {
   return (
     <BrowserRouter>
-      {/* Authentication */}
       <Routes>
+        {/* Authentication */}
         <Route path="/" element={<RegisterPage />} />
         <Route path="/register" element={<RegisterPage />} />
         <Route path="/login" element={<LoginPage />} />
-      </Routes>
 
-      {/* Main pages */}
-      <div>
-        <Routes>
+        {/* Main pages */}
+        <Route element={<MainLayout />}>
           <Route path="/dashboard" element={<DashboardPage />} />
-        </Routes>
-      </div>
+        </Route>
+      </Routes>
     </BrowserRouter>
   );
 };
