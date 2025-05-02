d68d5f8741a32682dc0c3e019fb8e0a7a78af7d5
usertop <tammasorn.tammasorn@mail.kmutt.ac.th>
2025-05-02 19:57:59 +0700

    feat(Dashboard): add 4 cards on dashboard page

--- my-plant-webapp/src/pages/DashboardPage.jsx
+++ my-plant-webapp/src/pages/DashboardPage.jsx
@@ -1,12 +1,40 @@
 import React from "react";
+import { motion } from "framer-motion";
+import { Thermometer, Droplet, Sun } from "lucide-react";
 
 // components
 import Header from "../components/common/Header";
+import Card from "../components/common/Card";
 
 const Dashboard = () => {
   return (
     <div className="flex-1 overflow-auto relative z-10">
       <Header title="Dashboard" />
+
+      <main className="max-w-7xl mx-auto py-6 px-4 lg:px-8">
+        {/* Values */}
+        <motion.div
+          className="grid grid-cols-1 gap-5 sm:grid-cols-2 lg:grid-cols-4 mb-8"
+          initial={{ opacity: 0, y: 20 }}
+          animate={{ opacity: 1, y: 0 }}
+          transition={{ duration: 1 }}
+        >
+          <Card
+            name="Temperature (outside)"
+            icon={Thermometer}
+            value="31 °C"
+            color="#6366F1"
+          />
+          <Card
+            name="Temperature (inside)"
+            icon={Thermometer}
+            value="25 °C"
+            color="#6366F1"
+          />
+          <Card name="Humidity" icon={Droplet} value="70 %" color="#6366F1" />
+          <Card name="Light" icon={Sun} value="ON" color="#6366F1" />
+        </motion.div>
+      </main>
     </div>
   );
 };
