f5da63e8f873deca198cadd36b6d7e977fd04b5c
usertop <tammasorn.tammasorn@mail.kmutt.ac.th>
2025-05-02 17:12:02 +0700

    feat(Sidbar): create header common component and add it to dashboard page

--- my-plant-webapp/src/pages/DashboardPage.jsx
+++ my-plant-webapp/src/pages/DashboardPage.jsx
@@ -1,64 +1,12 @@
-import React, { useState, useEffect } from "react";
-import { useNavigate } from "react-router-dom";
-import { auth, db } from "../firebase/config.js";
-import { doc, getDoc } from "firebase/firestore";
+import React from "react";
+
+// components
+import Header from "../components/common/Header";
 
 const Dashboard = () => {
-  const [userDetails, setUserDetails] = useState(null);
-
-  let navigate = useNavigate();
-
-  const fetchUserData = async () => {
-    auth.onAuthStateChanged(async (user) => {
-      if (user) {
-        console.log("user: ", user);
-        const docRef = doc(db, "Users", user.uid);
-        const docSnap = await getDoc(docRef);
-        if (docSnap.exists()) {
-          setUserDetails(docSnap.data());
-          console.log("Doc snap data: ", docSnap.data());
-        } else {
-          console.log("User document not found");
-        }
-      } else {
-        console.log("User is not logged in");
-        navigate("/login");
-      }
-    });
-  };
-  useEffect(() => {
-    fetchUserData();
-  }, []);
-
-  const handleLogout = async () => {
-    try {
-      await auth.signOut();
-      navigate("/login");
-      console.log("User logged out successfully!!");
-    } catch (error) {
-      console.error("Error logging out: ", error);
-    }
-  };
-
   return (
-    <div>
-      {userDetails ? (
-        <>
-          <h3>Welcome {userDetails.firstname} 👋🏼</h3>
-          <div>
-            <p>Email: {userDetails.email}</p>
-            <p>First name: {userDetails.firstname}</p>
-            <p>Last name: {userDetails.lastname}</p>
-          </div>
-          <button style={{ backgroundColor: "red" }} onClick={handleLogout}>
-            Logout
-          </button>
-        </>
-      ) : (
-        <>
-          <h3>Loading...</h3>
-        </>
-      )}
+    <div className="flex-1 overflow-auto relative z-10">
+      <Header title="Dashboard" />
     </div>
   );
 };
