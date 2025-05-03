acd3acb89baffe58c28098a5a8f2041f8ad754f1
usertop <tammasorn.tammasorn@mail.kmutt.ac.th>
2025-05-03 19:16:03 +0700

    feat(ProfilePage): create CardProfile and CardPhoto

--- my-plant-webapp/src/components/common/Card.jsx
+++ my-plant-webapp/src/components/common/Card.jsx
@@ -26,15 +26,44 @@ export const CardProfile = ({ name, icon: Icon, value, color, gif }) => {
       className="bg-gray-800 bg-opacity-50 backdrop-blur-md overflow-hidden shadow-lg rounded-xl border border-gray-700"
       whileHover={{ y: -5, boxShadow: "0 25px 50px -12px rgba(0, 0, 0, 0.5)" }}
     >
-      <div className="px-4 py-5 sm:p-6">
-        <span className="flex items-center text-sm font-medium text-gray-400">
-          <img src={gif} alt="icon" className="w-6 h-6 mr-2" />
-          {name}
-        </span>
-        <p className={`mt-1 text-3xl font-semibold text-gray-100 ${color}`}>
-          {value}
-        </p>
+      <div className="px-4 py-5 sm:p-6 space-y-2">
+        {/* First name */}
+        <div className="flex flex-wrap items-center text-sm font-medium">
+          <div className="text-white min-w-[100px]">First name:</div>
+          <div className="flex-1 text-gray-400 ml-2 bg-black rounded-md ">
+            <div className="p-1 ml-2">{name}</div>
+          </div>
+        </div>
+        {/* Last name */}
+        <div className="flex flex-wrap items-center text-sm font-medium">
+          <div className="text-white min-w-[100px]">First name:</div>
+          <div className="flex-1 text-gray-400 ml-2 bg-black rounded-md">
+            <div className="p-1 ml-2">{name}</div>
+          </div>
+        </div>
+        {/* Email */}
+        <div className="flex flex-wrap items-center text-sm font-medium">
+          <div className="text-white min-w-[100px]">First name:</div>
+          <div className="flex-1 text-gray-400 ml-2 bg-black rounded-md">
+            <div className="p-1 ml-2">{name}</div>
+          </div>
+        </div>
       </div>
+    </motion.div>
+  );
+};
+
+export const CardPhoto = ({ className = "" }) => {
+  return (
+    <motion.div
+      className={`bg-gray-800 bg-opacity-50 backdrop-blur-md overflow-hidden shadow-lg rounded-xl border border-gray-700 ${className}`}
+      whileHover={{ y: -5, boxShadow: "0 25px 50px -12px rgba(0, 0, 0, 0.5)" }}
+    >
+      <img
+        src="src/assets/icons/profile.png"
+        alt="profile"
+        className="w-full h-auto object-cover"
+      />
     </motion.div>
   );
 };
--- my-plant-webapp/src/pages/ProfilePage.jsx
+++ my-plant-webapp/src/pages/ProfilePage.jsx
@@ -3,7 +3,7 @@ import { motion } from "framer-motion";
 
 // components
 import Header from "../components/common/Header";
-import { CardParameter } from "../components/common/Card";
+import { CardProfile, CardPhoto } from "../components/common/Card";
 
 const ProfilePage = () => {
   return (
@@ -12,13 +12,13 @@ const ProfilePage = () => {
 
       <main className="max-w-7xl mx-auto py-6 px-4 lg:px-8">
         <motion.div
-          className="grid grid-cols-[2fr_1fr] gap-5 mb-8"
+          className="grid grid-cols-[2fr_auto] gap-5 mb-8"
           initial={{ opacity: 0, y: 20 }}
           animate={{ opacity: 1, y: 0 }}
           transition={{ duration: 1 }}
         >
-          <CardParameter Hello />
-          <CardParameter Hello />
+          <CardProfile name="Tammasorn" value={60} />
+          <CardPhoto className="w-[250px]" />
         </motion.div>
       </main>
     </div>
