66e3d13839b0002240226e3f60fd24ac2c20a997
usertop <tammasorn.tammasorn@mail.kmutt.ac.th>
2025-05-03 15:19:48 +0700

    feat(ProfilePage): add header to the page

--- my-plant-webapp/src/pages/ProfilePage.jsx
+++ my-plant-webapp/src/pages/ProfilePage.jsx
@@ -1,7 +1,14 @@
 import React from "react";
 
+// components
+import Header from "../components/common/Header";
+
 const ProfilePage = () => {
-  return <div>ProfilePage</div>;
+  return (
+    <div className="flex-1 overflow-auto relative z-10">
+      <Header title="Profile" />
+    </div>
+  );
 };
 
 export default ProfilePage;
