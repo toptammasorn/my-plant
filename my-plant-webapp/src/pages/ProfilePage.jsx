34fed8dbe9a80d60e9def2689baf5fd4d60e1a6c
usertop <tammasorn.tammasorn@mail.kmutt.ac.th>
2025-05-03 20:28:27 +0700

    fix(ProfilePage): add conditon to display else component if user information are not ready

--- my-plant-webapp/src/pages/ProfilePage.jsx
+++ my-plant-webapp/src/pages/ProfilePage.jsx
@@ -49,26 +49,30 @@ const ProfilePage = () => {
           {/* <CardProfile cardName="First name" information="Apiwat" />
           <CardProfile cardName="Last name" information="Taninipong" /> */}
 
-          <motion.div
-            className="bg-gray-800 bg-opacity-50 backdrop-blur-md overflow-hidden shadow-lg rounded-xl border border-gray-700"
-            whileHover={{
-              y: -5,
-              boxShadow: "0 25px 50px -12px rgba(0, 0, 0, 0.5)",
-            }}
-          >
-            <div className="px-4 py-5 sm:p-6 space-y-2">
-              {/* User information cards */}
-              <CardProfile
-                cardName="First name"
-                information={userDetails.firstname}
-              />
-              <CardProfile
-                cardName="Last name"
-                information={userDetails.lastname}
-              />
-              <CardProfile cardName="Email" information={userDetails.email} />
-            </div>
-          </motion.div>
+          {userDetails ? (
+            <motion.div
+              className="bg-gray-800 bg-opacity-50 backdrop-blur-md overflow-hidden shadow-lg rounded-xl border border-gray-700"
+              whileHover={{
+                y: -5,
+                boxShadow: "0 25px 50px -12px rgba(0, 0, 0, 0.5)",
+              }}
+            >
+              <div className="px-4 py-5 sm:p-6 space-y-2">
+                {/* User information cards */}
+                <CardProfile
+                  cardName="First name"
+                  information={userDetails.firstname}
+                />
+                <CardProfile
+                  cardName="Last name"
+                  information={userDetails.lastname}
+                />
+                <CardProfile cardName="Email" information={userDetails.email} />
+              </div>
+            </motion.div>
+          ) : (
+            <div>Loading...</div>
+          )}
           <CardPhoto className="w-[250px]" />
         </motion.div>
       </main>
