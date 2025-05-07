4c4272a41132d77a97d0bf76648bb94c4912bc42
usertop <tammasorn.tammasorn@mail.kmutt.ac.th>
2025-05-07 21:08:41 +0700

    build(Firebase): import RTDB

--- my-plant-webapp/src/firebase/config.js
+++ my-plant-webapp/src/firebase/config.js
@@ -3,6 +3,7 @@ import { initializeApp } from "firebase/app";
 import { getAnalytics } from "firebase/analytics";
 import { getAuth } from "firebase/auth";
 import { getFirestore } from "firebase/firestore";
+import { getDatabase } from "firebase/database";
 
 // TODO: Add SDKs for Firebase products that you want to use
 // https://firebase.google.com/docs/web/setup#available-libraries
@@ -22,6 +23,7 @@ const firebaseConfig = {
 // Initialize Firebase
 const app = initializeApp(firebaseConfig);
 const analytics = getAnalytics(app);
+const database = getDatabase(app);
 
 export const auth = getAuth(app);
 export const db = getFirestore(app);
