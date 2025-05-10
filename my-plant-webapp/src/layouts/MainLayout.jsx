bfbeb3dcba9df1af3b634b6915baada40bf33a6a
usertop <tammasorn.tammasorn@mail.kmutt.ac.th>
2025-05-10 14:16:14 +0700

    feat(dashboard): change background

--- my-plant-webapp/src/layouts/MainLayout.jsx
+++ my-plant-webapp/src/layouts/MainLayout.jsx
@@ -6,9 +6,14 @@ import Sidebar from "../components/Sidebar";
 
 const MainLayout = () => {
   return (
-    <div className="flex h-screen bg-gray-900 text-gray-100 overflow-hidden">
+    <div
+      className="flex h-screen bg-gray-900 text-gray-100 overflow-hidden"
+      style={{
+        backgroundImage: "url('../src/assets/backgrounds/Shire.jpg')",
+      }}
+    >
       <div className="fixed inset-0 z-0">
-        <div className="absolute inset-0 bg-gradient-to-br from-gray-900 via-gray-800 to-gray-900 opacity-80" />
+        <div className="absolute inset-0 bg-gradient-to-br from-gray-900 via-gray-800 to-gray-900 opacity-60" />
         <div className="absolute inset-0 backdrop-blur-sm" />
       </div>
 
