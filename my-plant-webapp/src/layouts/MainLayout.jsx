84630ca6b4976323240109b05a04041764752a16
usertop <tammasorn.tammasorn@mail.kmutt.ac.th>
2025-05-10 18:18:55 +0700

    fix(background): apply background opacity

--- my-plant-webapp/src/layouts/MainLayout.jsx
+++ my-plant-webapp/src/layouts/MainLayout.jsx
@@ -13,7 +13,7 @@ const MainLayout = () => {
       }}
     >
       <div className="fixed inset-0 z-0">
-        <div className="absolute inset-0 bg-gradient-to-br from-gray-900 via-gray-800 to-gray-900 opacity-" />
+        <div className="absolute inset-0 bg-gradient-to-br from-gray-900 via-gray-800 to-gray-900 opacity-60" />
         <div className="absolute inset-0 backdrop-blur-sm" />
       </div>
 
