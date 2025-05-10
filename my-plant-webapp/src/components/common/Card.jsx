034174a90c5cb727156f62bdba6986bce60ce954
usertop <tammasorn.tammasorn@mail.kmutt.ac.th>
2025-05-10 14:41:00 +0700

    refactor(Dashboard): change card color and transparent them

--- my-plant-webapp/src/components/common/Card.jsx
+++ my-plant-webapp/src/components/common/Card.jsx
@@ -11,7 +11,7 @@ export const CardParameter = ({
 }) => {
   return (
     <motion.div
-      className="bg-gray-800 bg-opacity-50 backdrop-blur-md overflow-hidden shadow-lg rounded-xl border border-gray-700"
+      className="bg-white/10 backdrop-blur-md overflow-hidden shadow-lg rounded-xl border border-gray-700"
       whileHover={{ y: -5, boxShadow: "0 25px 50px -12px rgba(0, 0, 0, 0.5)" }}
     >
       <div className="px-4 py-5 sm:p-6">
